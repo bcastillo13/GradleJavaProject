3bfd2010d20578b913979bcfc0ab1746944ef772
Braian Castillo <brian.castillo.h@gmail.com>
2022-08-22 21:01:20 -0500

    Add files via upload

---
+++ firstJavaProject/app/src/main/java/com/bcastillo/gradle/Product.java
@@ -0,0 +1,22 @@
+package com.bcastillo.gradle;
+
+public class Product {
+    String name;
+    String description;
+    int price;
+
+    public Product(String name, String description, int price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
